bc3055ae2e9246998dba79fa027231285ca8ddbb
Guillaume Turri <guillaume.turri@gmail.com>
2011-07-12 19:45:31 +0200

    Ajout d'une gestion des erreurs autours de la lecture de la ligne de cmd

--- src/generateur/ParseCmdLine.cc
+++ src/generateur/ParseCmdLine.cc
@@ -3,17 +3,22 @@
 using namespace std;
 
 variables_map ParseCmdLine::parse(int argc_p, char** argv_p){
+    variables_map result_l;
     options_description desc_l("Options autorisees");
     desc_l.add_options()
         ("help", "genere ce message")
         ("strategy,s", value<string>(), "Specifie la strategy de generation a adopter");
 
-    variables_map result_l;
-    store(parse_command_line(argc_p, argv_p, desc_l), result_l);
-    notify(result_l);
+    try {
+        store(parse_command_line(argc_p, argv_p, desc_l), result_l);
+        notify(result_l);
 
-    if ( result_l.count("help") ){
-        cout << desc_l << endl;
+        if ( result_l.count("help") ){
+            cout << desc_l << endl;
+        }
+    }catch(exception& err_l) {
+        cerr << "Erreur de lecture de la ligne de commande : " << err_l.what() << endl;
+        cerr << desc_l << endl;
     }
 
     return result_l;
