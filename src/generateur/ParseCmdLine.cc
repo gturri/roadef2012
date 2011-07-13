50577f035bbf251166edc38567721995e9eb8f20
Guillaume Turri <guillaume.turri@gmail.com>
2011-07-13 07:54:38 +0200

    Ajout de l'option -h (alias de --help) au generateur

--- src/generateur/ParseCmdLine.cc
+++ src/generateur/ParseCmdLine.cc
@@ -6,7 +6,7 @@ variables_map ParseCmdLine::parse(int argc_p, char** argv_p){
     variables_map result_l;
     options_description desc_l("Options autorisees");
     desc_l.add_options()
-        ("help", "genere ce message")
+        ("help,h", "genere ce message")
         ("strategy,s", value<string>()->default_value("default"), "Specifie la strategy de generation a adopter")
         ("writer,w", value<string>()->default_value("default"), "Specifie le format de la sortie");
 
