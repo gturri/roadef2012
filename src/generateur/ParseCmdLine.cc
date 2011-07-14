20b5c2cac6c557c4bce706a76aca02cc9447ca08
Guillaume Turri <guillaume.turri@gmail.com>
2011-07-14 09:29:18 +0200

    L'appel du generateur avec -h ne doit pas lancer de generation

--- src/generateur/ParseCmdLine.cc
+++ src/generateur/ParseCmdLine.cc
@@ -1,4 +1,5 @@
 #include "generateur/ParseCmdLine.hh"
+#include <cstdlib>
 #include <string>
 using namespace std;
 
@@ -6,7 +7,7 @@ variables_map ParseCmdLine::parse(int argc_p, char** argv_p){
     variables_map result_l;
     options_description desc_l("Options autorisees");
     desc_l.add_options()
-        ("help,h", "genere ce message")
+        ("help,h", "genere ce message et arrete l'execution")
         ("strategy,s", value<string>()->default_value("default"), "Specifie la strategy de generation a adopter")
         ("writer,w", value<string>()->default_value("default"), "Specifie le format de la sortie")
         ("out,o", value<string>()->default_value(""), "Prefixe des fichiers a ecrire (peut eventuellement contenir un path)");
@@ -17,6 +18,7 @@ variables_map ParseCmdLine::parse(int argc_p, char** argv_p){
 
         if ( result_l.count("help") ){
             cout << desc_l << endl;
+            exit(0);
         }
     }catch(exception& err_l) {
         cerr << "Erreur de lecture de la ligne de commande : " << err_l.what() << endl;
