8f6f7ab085a1d04d2a8c4e51b07402c9775a6371
Guillaume Turri <guillaume.turri@gmail.com>
2011-07-20 08:20:36 +0200

    Completion du writer de solutions

--- src/dtoout/SolutionDtoout.cc
+++ src/dtoout/SolutionDtoout.cc
@@ -1,9 +1,11 @@
 #include "dtoout/SolutionDtoout.hh"
+#include "alg/ContextALG.hh"
 #include "bo/ContextBO.hh"
 #include "bo/MachineBO.hh"
 #include "bo/ProcessBO.hh"
 #include <fstream>
 #include <sstream>
+#include <boost/foreach.hpp>
 using namespace std;
 
 string SolutionDtoout::outFileName_m;
@@ -17,7 +19,7 @@ void SolutionDtoout::writeSolInit(ContextBO* pContextBO_p, const string& outFile
     if ( ! ofs_l ){
         ostringstream oss_l;
         oss_l << "Impossible d'ouvrir le flux de sortie " << outFileName_m
-            << "pour y ecrire une solution" << endl;
+            << " pour y ecrire une solution initiale" << endl;
         throw oss_l.str();
     }
 
@@ -28,4 +30,20 @@ void SolutionDtoout::writeSolInit(ContextBO* pContextBO_p, const string& outFile
             ofs_l << " ";
         }
     }
+}
+
+void SolutionDtoout::writeSol(const vector<int>& vSol_p){
+    ofstream ofs_l(outFileName_m.c_str());
+    if ( ! ofs_l ){
+        ostringstream oss_l;
+        oss_l << "Impossible d'ouvrir le flux de sortie " << outFileName_m
+            << " pour y ecrire une solution" << endl;
+        throw oss_l.str();
+    }
+
+    copy(vSol_p.begin(), vSol_p.end(), ostream_iterator<int>(ofs_l, " "));
+}
+
+void SolutionDtoout::writeSol(const ContextALG& contextALG_p){
+    writeSol(contextALG_p.getCurrentSol());
 }
