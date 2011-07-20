d383c75f39c2334ad1e0291b65d8c5f95344ff39
Guillaume Turri <guillaume.turri@gmail.com>
2011-07-20 18:41:45 +0200

    Il devient possible de passer des arguments au generateur d'instances via un fichier.
    Il suffit de passer au binaire l'option --optionFile (en ligne de commande....)

--- src/generateur/ParseCmdLine.cc
+++ src/generateur/ParseCmdLine.cc
@@ -1,8 +1,38 @@
 #include "generateur/ParseCmdLine.hh"
+#include "tools/Log.hh"
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
 #include <string>
 using namespace std;
+
+void ParseCmdLine::parseLine(variables_map& vm_p, const options_description& desc_p, int argc_p, char** argv_p){
+    try {
+        store(parse_command_line(argc_p, argv_p, desc_p), vm_p);
+        notify(vm_p);
+    }catch(exception& err_l) {
+        cerr << "Erreur de lecture de la ligne de commande : " << err_l.what() << endl;
+        cerr << desc_p << endl;
+        exit(-1);
+    }
+}
+
+void ParseCmdLine::parseCfgFile(variables_map& vm_p, const options_description& desc_p, const string& filename_p){
+    ifstream ifs_l(filename_p.c_str());
+    if ( ! ifs_l ){
+        LOG(WARNING) << "Impossible d'ouvrir le fichier d'options du generateur d'instances (" << filename_p << "). Celui-ci est ignore" << endl;
+        return;
+    }
+
+    try {
+        store(parse_config_file(ifs_l, desc_p), vm_p);
+        notify(vm_p);
+    }catch(exception& err_l) {
+        cerr << "Erreur de lecture du fichier d'options du generateur d'instances (" << filename_p << ") : " << err_l.what() << endl;
+        cerr << desc_p << endl;
+        exit(-1);
+    }
+}
 
 variables_map ParseCmdLine::parse(int argc_p, char** argv_p){
     variables_map result_l;
@@ -13,19 +43,17 @@ variables_map ParseCmdLine::parse(int argc_p, char** argv_p){
         ("writer,w", value<string>()->default_value("default"), "Specifie le format de la sortie")
         ("out,o", value<string>()->default_value(""), "Prefixe des fichiers a ecrire (peut eventuellement contenir un path)")
         ("fromExistingInst", value<string>(), "Dans le cas d'une generation depuis une instance existante, specifie le fichier decrivant cette instance initiale")
-        ("fromExistingSol", value<string>(), "Dans le cas d'une generation depuis une intance existante, specifie le fichier decrivant la solution initiale");
+        ("fromExistingSol", value<string>(), "Dans le cas d'une generation depuis une intance existante, specifie le fichier decrivant la solution initiale")
+        ("optionFile", value<string>(), "Nom d'un fichier contenant des options");
 
-    try {
-        store(parse_command_line(argc_p, argv_p, desc_l), result_l);
-        notify(result_l);
+    parseLine(result_l, desc_l, argc_p, argv_p);
+    if ( result_l.count("optionFile") ){
+        parseCfgFile(result_l, desc_l, result_l["optionFile"].as<string>());
+    }
 
-        if ( result_l.count("help") ){
-            cout << desc_l << endl;
-            exit(0);
-        }
-    }catch(exception& err_l) {
-        cerr << "Erreur de lecture de la ligne de commande : " << err_l.what() << endl;
-        cerr << desc_l << endl;
+    if ( result_l.count("help") ){
+        cout << desc_l << endl;
+        exit(0);
     }
 
     return result_l;
--- src/generateur/ParseCmdLine.hh
+++ src/generateur/ParseCmdLine.hh
@@ -1,11 +1,18 @@
 #ifndef PARSECMDLINE_HH
 #define PARSECMDLINE_HH
 #include <boost/program_options.hpp>
+#include <string>
+using namespace std;
 using namespace boost::program_options;
 
 class ParseCmdLine{
     public:
         static variables_map parse(int argc, char** argv);
+
+    protected:
+        static void parseLine(variables_map& vm_p, const options_description& desc_p,  int argc, char** argv);
+        static void parseCfgFile(variables_map& vm_p, const options_description& desc_p, const string& filename_p);
+
 };
 
 #endif
