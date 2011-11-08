3458749559a322552e3d6c42774dd915410cf4ac
Guillaume Turri <guillaume.turri@gmail.com>
2011-11-08 08:19:07 +0100

    Gitisation du checker officiel
    afin entre autre de savoir ou le trouver dans nos scripts d'evaluation

---
+++ solution_checker/comp.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -x
+CXX="ccache g++"
+CXXFLAGS="-O2"
+
+$CXX -c $CXXFLAGS solution_checker.cc
+$CXX -c $CXXFLAGS solution_checker_run.cc
+$CXX $CXXFLAGS solution_checker_run.o solution_checker.o -o solution_checker 
