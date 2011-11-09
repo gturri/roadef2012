019e534de2b3ad4770b58da4dcbdce2287e247a6
Guillaume Turri <guillaume.turri@gmail.com>
2011-11-09 21:14:23 +0100

    Utilisation de l'API de logging la ou elle devrait etre utilisee

--- src/main.cc
+++ src/main.cc
@@ -26,19 +26,19 @@ int main(int argc, char **argv) {
     try {
         /* Lecture du fichier d'instance et de solution initiale
          */
-        cerr << "reading instance" << endl;
+        LOG(INFO) << "reading instance" << endl;
         InstanceReaderDtoin reader_l;
         ContextBO contextBO_l = reader_l.read(opt_l["param"].as<string>());
         SolutionDtoin::read(opt_l["init"].as<string>(), &contextBO_l);
 
         /* Lancement de la sequence d'optim.
          */
-        cerr << "creating context" << endl;
+        LOG(INFO) << "creating context" << endl;
         ContextALG contextALG_l(&contextBO_l);
             //Initialise SolutionDtoout::bestSol_m
         contextALG_l.checkCompletAndMajBestSol(contextALG_l.getCurrentSol(), false);
         StrategyOptim* pStrategy_l = StrategySelecter::buildStrategy(opt_l);
-        cerr << "running method" << endl;
+        LOG(INFO) << "running method" << endl;
         pStrategy_l->run(contextALG_l, time(0) + opt_l["time"].as<int>(), opt_l);
 
         /* Risque de fuite de memoire : si une exception est levee pendant l'optim,
