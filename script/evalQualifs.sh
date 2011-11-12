7fa7faed4f6c1af8abc2c1d27819f115032a8328
Guillaume Pinot <guillaume.pinot@tremplin-utc.net>
2011-11-12 19:53:06 +0100

    nettoyage du script

--- script/evalQualifs.sh
+++ script/evalQualifs.sh
@@ -13,8 +13,8 @@ rm -f $FICHIER_SORTIE
 date > $FICHIER_SORTIE
 
 for i in 1 2; do
-    for j in $(seq 1 5); do
-      echo "Lancement du run $i $j(" $(date) ")"
+  for j in 1 2 3 4 5; do
+    echo "Lancement du run $i $j(" $(date) ")"
 
     SOL_INIT=../instances/assignment_a${i}_${j}.txt
     MODEL=../instances/model_a${i}_${j}.txt
@@ -26,26 +26,22 @@ for i in 1 2; do
     #Lancement du run
     ${PATH_BINAIRE}/machineReassignment -t ${TEMPS} -i $SOL_INIT -p $MODEL -s 0 -o benchOut.txt &
     sleep ${TEMPS}
-    killall machineReassignment
+    kill %%
 
     #Evaluation du score et log
     echo -n "\tFin du run $i $j"
-    SCORE_INITIAL=$(${PATH_EVALUATEUR} $MODEL $SOL_INIT $SOL_INIT | \grep "Solution is valid" | cut -d " " -f 8)
+    SCORE_INITIAL=$($PATH_EVALUATEUR $MODEL $SOL_INIT $SOL_INIT | grep "Solution is valid" | cut -d " " -f 8)
 
-    SCORE_FINAL=$(${PATH_EVALUATEUR} $MODEL $SOL_INIT benchOut.txt | \grep "Solution is valid" | cut -d " " -f 8)
+    SCORE_FINAL=$($PATH_EVALUATEUR $MODEL $SOL_INIT benchOut.txt | grep "Solution is valid" | cut -d " " -f 8)
 
     if [ "x$SCORE_FINAL" = "x" ]; then
-    {
       echo ""
       echo "*********************************************************"
       echo "!!! ATTENTION : une solution non valide a ete fournie !!!"
       echo "*********************************************************"
       SCORE_FINAL=$SCORE_INITIAL
-    }
     else
-    {
       echo " : Evolution du score : $SCORE_INITIAL -> $SCORE_FINAL"
-    }
     fi
 
     echo "$SOL_INIT $MODEL : $SCORE_INITIAL -> $SCORE_FINAL" >> $FICHIER_SORTIE
@@ -53,4 +49,4 @@ for i in 1 2; do
   done
 done
 
-rm benchOut.txt
+rm -f benchOut.txt
