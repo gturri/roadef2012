b5467f973d9a83f8411c7a14628b26198124bc8a
Guillaume Turri <guillaume.turri@gmail.com>
2012-03-07 20:26:50 +0100

    removing some scripts

--- solution_checker/comp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -x
-CXX="g++"
-CXXFLAGS="-O2"
-
-$CXX -c $CXXFLAGS solution_checker.cc
-$CXX -c $CXXFLAGS solution_checker_run.cc
-$CXX $CXXFLAGS solution_checker_run.o solution_checker.o -o solution_checker 
